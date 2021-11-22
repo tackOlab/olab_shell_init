a37fe5098a6a79aec04b17f9b4915df044a97aae
Osamu Watanabe <65328111+osamu620@users.noreply.github.com>
2021-11-22 17:46:36 +0900

    Update init.sh

--- init.sh
+++ init.sh
@@ -9,11 +9,17 @@ done
 
 brew tap homebrew/cask-fonts
 brew install --cask font-jetbrains-mono-nerd-font
-brew install peco tmux alacritty
+brew install peco tmux
 
 cp ./zshrc ~/.zshrc
 cp ./zpreztorc ~/.zpreztorc
 cp -r ./tmux ~/.tmux
 cp ./tmux.conf ~/.tmux.conf
 cp ./alacritty.yml ~/.alacritty.yml
+
+# curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+# rustup override set stable
+# rustup update stable
+
+git clone https://github.com/alacritty/alacritty.git $HOME/alacritty
 
